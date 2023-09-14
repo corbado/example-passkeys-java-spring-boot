5f548f23b0fabf64755a69a520e89fbb7e2a7bce
Nicolai Stein <nicolai_stein@outlook.de>
2023-09-14 10:30:35 +0200

    rename controller

--- complete/src/main/java/com/example/springboot/FrontendController.java
+++ complete/src/main/java/com/example/springboot/FrontendController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class FrontendController {
 
 	@Autowired
 	private Environment environment;

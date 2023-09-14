492db8094dd2223aa01d8daa50e4be72454af360
Nicolai Stein <nicolai_stein@outlook.de>
2023-09-14 18:08:05 +0200

    use templates and env variables

--- complete/src/main/java/com/corbado/springboot/FrontendController.java
+++ complete/src/main/java/com/corbado/springboot/FrontendController.java
@@ -1,100 +1,30 @@
 package com.corbado.springboot;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 
-@RestController
+@Controller
 public class FrontendController {
 
-	@Autowired
-	private Environment environment;
+	@Value("${projectid}")
+	private String projectID;
 
-	@GetMapping("/")
-	public String index() {
-		String projectID = environment.getProperty("environment.projectid");
-		return "<!DOCTYPE html>" +
-				"<html>" +
-				"" +
-				"<head>" +
-				"    <title>Corbado</title>" +
-				"</head>" +
-				"" +
-				"<body>" +
-				"<script src=\"https://" + projectID + ".frontendapi.corbado.io/auth.js\"></script>" +
-				"<corbado-auth-provider project-id=" + projectID + ">" +
-				"    <div slot=\"unauthed\">" +
-				"        <corbado-auth project-id=" + projectID + " conditional=\"yes\">" +
-				"            <input name=\"username\" id=\"corbado-username\" data-input=\"username\" required autocomplete=\"webauthn\" />" +
-				"        </corbado-auth>" +
-				"    </div>" +
-				"" +
-				"    <div slot=\"authed\">" +
-				"        You're logged in. Click <a href=\"/profile\">here</a> to visit the profile page." +
-				"    </div>" +
-				"" +
-				"</corbado-auth-provider>" +
-				"</body>" +
-				"" +
-				"</html>";
+	@RequestMapping("/")
+	public String index(Model model) {
+		model.addAttribute("PROJECT_ID", projectID);
+		return "index";
 	}
 
-
-	@GetMapping("/profile")
-	public String profile() {
-		String projectID = environment.getProperty("environment.projectid");
-		return "<!DOCTYPE html>" +
-				"<html>" +
-				"" +
-				"<head>" +
-				"    <title>Corbado</title>" +
-				"</head>" +
-				"<body>" +
-				"    <!-- Import Corbado SDK -->" +
-				"    <script src=\"https://" + projectID + ".frontendapi.corbado.io/auth.js\"></script>" +
-				"    <script src=\"https://" + projectID + ".frontendapi.corbado.io/utility.js\"></script>" +
-				"    <script>" +
-				"        const session = new Corbado.Session('" + projectID + "');" +
-				"        session.refresh(user => {" +
-				"            if (user === null) {" +
-				"            } else {" +
-				"                document.getElementById('info').innerHTML = `" +
-				"                    User-ID: ${user.userID}" +
-				"                    <br />" +
-				"                    Email: ${user.email}" +
-				"                `;" +
-				"                console.log(" +
-				"                    user.userID," +
-				"                    user.userIdentifier," +
-				"                    user.userFullName," +
-				"                    user.email," +
-				"                    user.phoneNumber" +
-				"                )" +
-				"            }" +
-				"        })" +
-				"    </script>" +
-				"    <corbado-auth-provider project-id=" + projectID + ">" +
-				"        <div slot=\"unauthed\">" +
-				"            This content is only available to logged in users. You can log in <a href=\"/\">here</a>." +
-				"        </div>" +
-				"" +
-				"        <div slot=\"authed\">" +
-				"            <div>" +
-				"                <h1>Profile Page</h1>" +
-				"                <div>" +
-				"                    <p id=\"info\"></p>" +
-				"                </div>" +
-				"            </div>" +
-				"            <corbado-logout-handler project-id=" + projectID + " redirect-url=\"/\">" +
-				"                <button>Logout</button>" +
-				"            </corbado-logout-handler>" +
-				"        </div>" +
-				"" +
-				"    </corbado-auth-provider>" +
-				"</body>" +
-				"" +
-				"</html>";
+	@RequestMapping("/home")
+	public String home(Model model, @CookieValue("cbo_short_session") String cboShortSession) {
+		model.addAttribute("PROJECT_ID", projectID);
+		model.addAttribute("USER_ID", projectID);
+		model.addAttribute("USER_NAME", cboShortSession);
+		model.addAttribute("USER_EMAIL", cboShortSession);
+		return "home";
 	}
-
 }
